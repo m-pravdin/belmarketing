506a52eb52e6f0c36f37456ed6182d2c1838729f
Maksym <m.pravdin@icloud.com>
2023-02-22 00:13:29 +0300

    Added setting for intro/cta blocks and script.js

--- source/js/script.js
+++ source/js/script.js
@@ -1,31 +1,12 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
-let popupBlock = document.querySelector('.popup-wrapper--closed');
-let popupButton = document.querySelector('.add-to-cart');
-let popupButtonAdd = document.querySelector('.popup__button');
-let mapImage = document.querySelector('.map__image-wrapper')
-
-navMain.classList.remove('main-nav--nojs');
+let navMain = document.querySelector('.nav');
+let navToggle = document.querySelector('.toggle__button');
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('nav--closed')) {
+    navMain.classList.remove('nav--closed');
+    navMain.classList.add('nav--opened');
     } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
+      navMain.classList.add('nav--closed');
+      navMain.classList.remove('nav--opened');
     }
 });
-
-if (popupButton) {
-  popupButton.addEventListener('click', function() {
-    if (popupBlock.classList.contains('popup-wrapper--closed')) {
-      popupBlock.classList.remove('popup-wrapper--closed');
-      popupBlock.classList.add('popup-wrapper--opened');
-      }
-  });
-}
-
-if(mapImage) {
-  mapImage.classList.add('visually-hidden')
-}
