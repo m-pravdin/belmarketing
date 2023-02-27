750d68b132d2ae8c55caf34f33c473ce87db0208
Maksym <m.pravdin@icloud.com>
2023-02-27 17:49:27 +0300

    Added basic markup

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,13 @@
 let navMain = document.querySelector('.nav');
 let navToggle = document.querySelector('.toggle__button');
 
+const tooltipToggle = document.querySelector(".tooltip-toggle");
+const tooltipWrapper = document.querySelector(".tooltip-wrapper");
+
+tooltipToggle.addEventListener("click", () => {
+  tooltipWrapper.classList.toggle("tooltip-wrapper--closed");
+});
+
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('nav--closed')) {
     navMain.classList.remove('nav--closed');
