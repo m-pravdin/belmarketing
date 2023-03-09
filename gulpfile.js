cb97c1525298a92afbfb1fd02852146deb80c478
Maksym <m.pravdin@icloud.com>
2023-03-10 02:17:55 +0300

    Added settings for desktop and mobile version

--- gulpfile.js
+++ gulpfile.js
@@ -45,13 +45,13 @@ return gulp.src('source/js/script.js')
 // Images
 
 const optimizeImages = () => {
-return gulp.src('source/img/**/*.{png,jpg}')
+return gulp.src('source/img/**/*.{png,jpg,gif}')
 .pipe(squoosh())
 .pipe(gulp.dest('build/img'))
 }
 
 const copyImages = () => {
-return gulp.src('source/img/**/*.{png,jpg}')
+return gulp.src('source/img/**/*.{png,jpg,gif}')
 .pipe(gulp.dest('build/img'))
 }
 
